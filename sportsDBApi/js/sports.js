0d28d597cda326f01c176f51068886c5a8a5fc4b
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-10-14 23:03:24 +0600

    server error handle okay

--- sportsDBApi/js/sports.js
+++ sportsDBApi/js/sports.js
@@ -1,23 +1,50 @@
 const searchButton = () =>{
     const inputText = document.getElementById("searchInput");
-    console.log(inputText.value);
     if(inputText.value==''){
         errorMessage('');
     }
+    else{
+        const loadDataV = loadData(inputText.value); 
+    }
     inputText.value = "";
 }
+
+
 
 const eMessage = document.getElementById("errorMessage");
 const clearContent = () =>{
     eMessage.textContent = '';
 }
 
-const errorMessage = (inputValue) =>{
-    clearContent();
-    const h2 = document.createElement('h2');
-    h2.classList.add("text-center", "text-danger");
-    if(inputValue==''){
-        h2.innerText = "Please enter something to get a result";
+const loadData = async (inputValue) =>{
+    try{
+        const url = `https://www.thesportsdb.com/api/v31/json/2/search_all_leagues.php?c=${inputValue}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.countries);
+        if(data.countries==null){
+            errorMessage('null');
+        }
     }
-    eMessage.appendChild(h2);
+    catch(error){
+        errorMessage(error);
+    }
+    
 }
+
+const errorMessage = (inputValue) => {
+  clearContent();
+  const h2 = document.createElement("h2");
+  h2.classList.add("text-center", "text-danger");
+  if (inputValue == "") {
+    h2.innerText = "Please enter something to get a result";
+  }
+  else if(inputValue=='null'){
+    h2.innerText = "No result found";
+  }
+  else{
+    h2.innerText = 'Something went wrong with the server';
+    console.log(inputValue);
+  }
+  eMessage.appendChild(h2);
+};
