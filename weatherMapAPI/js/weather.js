b0e2120ba4a503c2c3c0faa4918100fc52c895a0
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-10-15 15:08:18 +0600

    weather map api complete

--- weatherMapAPI/js/weather.js
+++ weatherMapAPI/js/weather.js
@@ -1,8 +1,11 @@
 const searchButton = () =>{
     const inputField = document.getElementById("inputField");
-    console.log(inputField.value);
+    // console.log(inputField.value);
     if(inputField.value==''){
         errorMessage('');
+    }
+    else{
+        loadData(inputField.value);
     }
 
     inputField.value = '';
@@ -11,9 +14,12 @@ const searchButton = () =>{
 
 
 const errorMessageS = document.getElementById("errorMessageS");
+const weatherInfo = document.getElementById("weatherInfo");
+const image = "http://127.0.0.1:5500/weatherMapAPI/images/02d@2x.png";
 
 const clearContent = () => {
     errorMessageS.textContent = '';
+    weatherInfo.textContent = '';
 }
 
 const errorMessage = (message) =>{
@@ -23,6 +29,36 @@ const errorMessage = (message) =>{
     if(message==''){
         h1.innerText = 'Please enter something to get a result';
     }
+    else{
+        h1.innerText = message;
+    }
 
     errorMessageS.appendChild(h1);
+}
+
+const loadData = async (city) =>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=278de88460b27cf7d073de2541ab61f8&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    if(data.cod=='404'){
+        errorMessage(data.message);
+    }
+    else if(data.cod=='200'){
+        displayData(data);
+    }
+    // console.log(data);
+}
+
+const displayData = (weather) => {
+    clearContent();
+    weatherInfo.innerHTML = `
+        <div class="mb-3">
+            <img src="${image}" alt="weatherImage">
+        </div>
+        <div>
+            <h1>${weather.name}</h1>
+            <h3>${weather.main.temp}°C</h3>
+            <h5>${weather.weather[0].description}</h5>
+        </div>
+    `;
 }
