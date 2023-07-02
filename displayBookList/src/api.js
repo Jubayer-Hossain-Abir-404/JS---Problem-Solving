ea4853207959ab34066c6b4e3306bf6c4c67091c
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-07-02 20:09:26 +0600

    card and container ready, important data fetch done

--- displayBookList/src/api.js
+++ displayBookList/src/api.js
@@ -1,7 +1,21 @@
+const displayData = (books, totalCount) =>{
+    console.log(totalCount);
+    books.forEach(book => {
+        console.log(
+          book.cover_i,
+          book.title,
+          book.subtitle,
+          book.author_name,
+          book.first_publish_year
+        );
+    });
+    
+}
+
 const loadData = (bookInfo) =>{
     fetch(`https://openlibrary.org/search.json?q=${bookInfo}`)
-    .then(res => res.json())
-    .then(data => console.log(data));
+      .then((res) => res.json())
+      .then((data) => displayData(data.docs, data.numFound));
 }
 
 loadData("javascript");
